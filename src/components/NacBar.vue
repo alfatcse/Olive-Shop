ac8686fdd034988da8fc41de0d81ae41981bf97e
Alfat <alfat.cse.21@gmail.com>
2023-07-05 21:41:42 +0200

    Navbar added

---
+++ src/components/NacBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="nav-bar">
+    <div class="logo-warp">
+      <img :src="logo" />
+    </div>
+    <button>Shopping Cart</button>
+  </div>
+</template>
+<script>
+import logo from "../assets/logo-hexagon";
+export default {
+  name: "NavBar",
+  data() {
+    return { logo };
+  },
+};
+</script>
--- src/pages/ShoppingCartPage.vue
+++ src/pages/ShoppingCartPage.vue
@@ -1,8 +1,29 @@
 <template>
-    <h1>Shopping Cart Page</h1>
+  <h1>Shopping Cart Page</h1>
+  <div v-if="cartItems.length > 0">
+    <div
+      class="product-container"
+      :key="product.id"
+      v-for="product in cartItems"
+    >
+      <img class="product-image" :src="product.imageName" />
+      <div class="details-warp">
+        <h3>{{ product.name }}</h3>
+        <p>{{ product.price }}</p>
+      </div>
+      <button class="remove-button">Remove from Cart</button>
+    </div>
+    <button class="checkout-button">Proceed to Checkout</button>
+  </div>
+  <div v-if="cartItems.length === 0">No items Available</div>
 </template>
 <script>
-export default{
-    name:"ShoppingCartPage",
-}
+// eslint-disable-next-line no-unused-vars
+import { cartItems } from "../temp-data";
+export default {
+  name: "ShoppingCartPage",
+  data() {
+    return { cartItems };
+  },
+};
 </script>
